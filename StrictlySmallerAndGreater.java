f93133ccff618967cba6e743e179aefdce1fe287
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-29 01:09:38 +0530

    Create StrictlySmallerAndGreater.java

---
+++ StrictlySmallerAndGreater.java
@@ -0,0 +1,10 @@
+# 2148
+  
+  class Solution {
+    public int countElements(int[] nums) {
+        final int min = Arrays.stream(nums).min().getAsInt();
+    final int max = Arrays.stream(nums).max().getAsInt();
+    return (int) Arrays.stream(nums).filter(num -> min < num && num < max).count();
+        
+    }
+}
