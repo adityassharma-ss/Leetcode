f4c9d12b01b2b586bbe37185e3d1c19525e74305
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-09-05 12:17:50 +0530

    Create SortCharactersByFrequency.java

---
+++ SortCharactersByFrequency.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String frequencySort(String S) {
+        Map<Character, Integer> characterCount = new HashMap<>();
+        for (char ch : S.toCharArray()) {
+            characterCount.put(ch, characterCount.getOrDefault(ch, 0) + 1);
+        }
+        List<Character> charactersList = new ArrayList<>(characterCount.keySet());        
+        Collections.sort(charactersList, (ch1, ch2) -> characterCount.get(ch2) - characterCount.get(ch1));
+        StringBuilder result = new StringBuilder();
+        for (char ch : charactersList) {
+            int charCount = characterCount.get(ch);
+            for (int i = 0; i < charCount; i++) {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+    }
