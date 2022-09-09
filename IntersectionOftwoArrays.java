086f0f5169ead4730d7ea7abb980af05751c38e2
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-09-09 19:32:53 +0530

    Update IntersectionOftwoArrays.java

--- IntersectionOftwoArrays.java
+++ IntersectionOftwoArrays.java
@@ -24,3 +24,48 @@ class Solution {
         
     }
 }
+
+// Approach 2: Sort one array and use binary search to insert elements
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Set<Integer> intersect = new HashSet();
+        
+        for(int num:nums2){
+            if(find(num,nums1)){
+                intersect.add(num);
+        }
+        }
+        
+        int[] result = new int[intersect.size()];
+        
+        int i=0;
+        
+        for(int num : intersect){
+            result[i++] = num;
+        }
+        return result;
+    }
+    
+    public boolean find(int target, int[] nums){
+        int left =0;
+        int right = nums.length-1;
+        
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            
+            if(nums[mid]==target){
+                return true;
+            }
+            
+            else if(nums[mid] > target){
+                right = mid -1;
+            }
+            else{
+                left = mid +1;
+            }
+        }
+        return false;
+    }
+}
