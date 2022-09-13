89e9b09660a989d17afd952be933c1c77a2abd23
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-09-13 13:51:01 +0530

    Create MoveZeros.java

---
+++ MoveZeros.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        while (k < nums.length) {
+          if (nums[k] == 0) {
+    
+            break;
+          } else {
+            k = k + 1;
+          }
+        }
+        int i = k, j = k + 1;
+    
+        while (i < nums.length && j < nums.length) {
+          if (nums[j] != 0) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+    
+          }
+    
+          j++;
+    
+        }
+    }
+}
