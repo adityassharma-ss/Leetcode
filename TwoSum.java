53a3c8d8be9dfb586bbac1f093abe0ad6d134c4f
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-09-14 23:08:36 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,5 @@
+// Approach 1: O(n^2)
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -12,5 +14,22 @@ class Solution {
         }
         return ans;
         
+    }
+}
+
+// Approach 2: Mapping: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[] {map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException();
     }
 }
