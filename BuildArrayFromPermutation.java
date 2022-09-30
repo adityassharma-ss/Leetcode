dcdfc5e28259766665177de340bfc3277d408d36
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-01 01:38:13 +0530

    Update BuildArrayFromPermutation.java

--- BuildArrayFromPermutation.java
+++ BuildArrayFromPermutation.java
@@ -1,5 +1,19 @@
 // 1920
 
+// Approach 1: 
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int ans[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+           ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
+
+//Approach 2: Optimised
 class Solution {
   public int[] buildArray(int[] nums) {
     final int n = nums.length;
@@ -13,3 +27,4 @@ class Solution {
     return nums;
   }
 }
+
