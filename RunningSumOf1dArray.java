b70c6d960098ed76ff15371e91c98784133fcc48
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-01 01:53:52 +0530

    Update RunningSumOf1dArray.java

--- RunningSumOf1dArray.java
+++ RunningSumOf1dArray.java
@@ -9,3 +9,17 @@ class Solution {
     return ans;
     }
 }
+
+// Optimised Approach:
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+    int n = nums.length;
+  
+    for(int i=1;i<n;i++){
+        nums[i] = nums[i]+nums[i-1];
+    }
+        return nums;
+    }
+}
+
