7efa1996ff2fd229651c94762577e2c0dafd020f
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-09-30 13:22:13 +0530

    Create CloneGraph.cpp

---
+++ CloneGraph.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return nullptr;
+        unordered_map<Node*, Node*> map{{node, new Node(node->val)}};
+        queue<Node*> q{{node}};
+        
+        while(!q.empty()){
+            Node* u = q.front();
+            q.pop();
+            for(Node* v : u->neighbors){
+                if (!map.count(v)) {
+          map[v] = new Node(v->val);
+          q.push(v);
+        }
+        map[u]->neighbors.push_back(map[v]);
+            }
+        }
+        return map[node];
+    }
+};
