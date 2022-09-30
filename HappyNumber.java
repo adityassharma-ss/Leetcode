3ca24efed3f4d658b7cca1083673ee4751ef60b4
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-09-30 20:25:08 +0530

    Update HappyNumber.java

--- HappyNumber.java
+++ HappyNumber.java
@@ -2,26 +2,24 @@
 
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-
-        do {
-            slow = findSquare(slow);
-            fast = findSquare(findSquare(fast));
-        } while (slow != fast);
-
-        if (slow == 1) {
-            return true;
-        }
+        int slow=n;
+        int fast=n;
+        do{
+            slow=sum(slow);
+            fast=sum(sum(fast));
+            if(slow==1)
+                return true;
+        }while(slow!=fast);
         return false;
     }
-    private int findSquare(int number) {
-        int ans = 0;
-        while (number > 0) {
-            int rem = number % 10 ;
-            ans += rem * rem;
-            number /= 10;
-        }
-        return ans;
-    }
+     public int sum(int n)
+     {
+         int sum =0;
+         while(n>0){
+         int rem = n%10;
+         sum+=(rem*rem);
+         n=n/10;
+         }
+         return sum;
+     }
 }
