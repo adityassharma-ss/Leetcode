da95f569d4b1f7f885811a760fdaf48c9bb96a4e
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-01 17:57:40 +0530

    Create CheckIfNAndItsDoubleExist.java

---
+++ CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,27 @@
+// Approach 1: SOrting With Binary Search || TC: O(nlogn) || SC: O(1)
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            int target=2*arr[i];
+            int start=0;
+            int end=arr.length-1;
+            while(start<=end ){
+                int mid=start+(end-start)/2;
+                if(arr[mid]==target&& mid!=i){
+                    return true;
+                }
+                else if(arr[mid]<target){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+            
+        }
+        return false;
+    }
+}
