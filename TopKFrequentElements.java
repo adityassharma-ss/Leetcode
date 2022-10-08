4ed34e7d187967453df570743574943b6f2c8d61
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-08 18:47:16 +0530

    Update TopKFrequentElements.java

--- TopKFrequentElements.java
+++ TopKFrequentElements.java
@@ -1,3 +1,29 @@
+// Approach Using Map & Priority Queue:
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length == k) return nums;
+        
+        Map<Integer,Integer> cmap = new HashMap<>();
+        
+        // it stores frequency of each element
+        for(int i: nums)
+            cmap.put(i, cmap.getOrDefault(i,0)+1);
+        
+        Queue<Integer> que = new PriorityQueue<>(k, (a,b) -> cmap.get(a)-cmap.get(b));
+
+        for(int i: cmap.keySet()){
+            que.add(i);
+            if(que.size()>k)
+                que.poll();
+        }
+        
+        return que.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
+
+// Approach 2:
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         ArrayList<Integer> res = new ArrayList<>();
