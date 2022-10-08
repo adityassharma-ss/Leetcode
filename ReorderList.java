807cc55e729e595ed9864ae62cf7fd5051275fa9
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-08 17:19:37 +0530

    Update ReorderList.java

--- ReorderList.java
+++ ReorderList.java
@@ -8,60 +8,78 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Approach 1: Split Linked List, Find Mid, Reverse Second Part, Then Reorder
+// TC: O(n) || SC: O(1)
 class Solution {
-    
-    public ListNode middleNode(ListNode head) {
-        ListNode s = head;
-        ListNode f = head;
-
-        while (f != null && f.next != null) {
-            s = s.next;
-            f = f.next.next;
+    public ListNode midNode(ListNode head){
+        ListNode fast =  head, slow  =  head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast =  fast.next.next;
+            slow =  slow.next;
         }
-        return s;
+        return slow;
     }
-    public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return head;
-        }
-        ListNode prev = null;
-        ListNode present = head;
-        ListNode next = present.next;
-
-        while (present != null) {
-            present.next = prev;
-            prev = present;
-            present = next;
-            if (next != null) {
-                next = next.next;
-            }
+    
+    public ListNode reverse(ListNode head){
+        ListNode curr =  head, prev=  null, next = null;
+        while(curr!=null){
+            next  =  curr.next;
+            curr.next =  prev ;
+            prev =  curr;
+            curr  =  next;
         }
         return prev;
     }
+
+    
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
+        
+        ListNode midNode =  midNode(head);
+        ListNode nextToMid =  midNode.next;
+        midNode.next = null;
+        ListNode p2 =  reverse(nextToMid);
+        
+        ListNode p1 =  head ,p1Next;    
+        while(p1!=null && p2!=null){
+            p1Next =  p1.next;                                    
+			p1.next =  p2;
+			           
+            p1 =  p2;
+            p2=p1Next;                        
+        }                
+    }            
+}
+
+// Approach 2: Single Traversal Approch || TC O(N) , SC O(n)
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode [] left =  new ListNode[1];// it will create in heap
+        left[0]  =  head;
+        reorder(left,head);
+        
+    }
+    
+    // left pointer will be created in heap and right pointer will be created in stack
+     public void reorder(ListNode left[],ListNode right){
+        if(right==null){
+            return ;
         }
-
-        ListNode mid = middleNode(head);
-
-        ListNode hs = reverseList(mid);
-        ListNode hf = head;
-
-        // rearrange
-        while (hf != null && hs != null) {
-            ListNode temp = hf.next;
-            hf.next = hs;
-            hf = temp;
-
-            temp = hs.next;
-            hs.next = hf;
-            hs = temp;
+        reorder(left,right.next);
+        
+    // in post area of recursion right pointer coming back(because of function remove from recursion stack)
+   // and we move left pointer forward 
+        if(left[0].next!=null){
+            ListNode leftNext =  left[0].next;
+            left[0].next =  right;
+            right.next =  leftNext;
+            left[0] =  leftNext;            
         }
-
-        // next of tail to null
-        if (hf != null) {
-            hf.next = null;
-        }
+        
+    // as we need to  merge till left pointer behind the right pointer 
+        if(left[0].next == right){
+            left[0].next =  null;
+        }                        
     }
 }
