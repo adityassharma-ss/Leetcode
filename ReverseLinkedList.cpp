6bf64ea518a49d4953f8b0b1d66ef6f984e2fa1b
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-12 19:30:15 +0530

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -22,3 +22,19 @@ public:
         return prev;
     }
 };
+
+// Approach 2: Recursion
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+        return head;
+    
+        ListNode* p = reverseList(head->next);
+
+        head->next->next = head;
+        head->next = NULL;
+        return p;
+    }
+};
