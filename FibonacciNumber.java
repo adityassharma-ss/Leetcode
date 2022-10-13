12c49caa7611cb4bef01efca1c0fedf8591f7ac5
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-13 16:27:34 +0530

    Update FibonacciNumber.java

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -6,3 +6,19 @@ class Solution {
         return fib(n-1)+fib(n-2);
     }
 }
+
+
+// Approach 2: Dynammic Prog
+
+class Solution {
+    public int fib(int n) {
+        if(n==0||n==1)
+            return n;
+        int []arr = new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++)
+            arr[i]= arr[i-1]+arr[i-2];
+        return arr[n];
+    }
+}
