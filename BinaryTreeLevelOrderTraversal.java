454bae91e55b73f36a4c98eaa715de41f255103e
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-15 16:15:18 +0530

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -22,3 +22,22 @@ class Solution {
     return ans;
   }
 }
+
+// Approach 2: Recursion
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        levelorder( root, 0 );
+        return ans;
+    }
+       public void levelorder(TreeNode root, int level){
+           if (root == null) return;
+           if (ans.size() < level+1)
+               ans.add(new ArrayList<>());
+           ans.get(level).add(root.val);
+           
+           levelorder(root.left, level+1);
+           levelorder(root.right, level+1);
+       }
+}
