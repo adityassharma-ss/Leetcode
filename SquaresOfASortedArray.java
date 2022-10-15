f0808d0549d7d74e79474ee1abdcd13a4a39b57e
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-15 16:57:47 +0530

    Update SquaresOfASortedArray.java

--- SquaresOfASortedArray.java
+++ SquaresOfASortedArray.java
@@ -18,3 +18,25 @@ class Solution {
         return res;
     }
 }
+
+// App2:
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int i = 0, j = n - 1;
+        
+        for (int k = n - 1; k >= 0; k--) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                res[k] = nums[i] * nums[i];
+                i++;
+            } else {
+                res[k] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return res;
+    }
+    }
+
