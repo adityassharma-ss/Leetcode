56d2191d4d4d93f44f0905424260718c1f44da2a
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-15 17:00:11 +0530

    Create SquaresOfASortedArray.cpp

---
+++ SquaresOfASortedArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        int i = 0, j = n - 1;
+        
+        for (int k = n - 1; k >= 0; k--) {
+            if (abs(nums[i]) > abs(nums[j])) {
+                res[k] = nums[i] * nums[i];
+                i++;
+            } else {
+                res[k] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return res;
+    }
+};
