2f4a45c6c87fd7ac9b212143c4b824364b2fad6e
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-10-18 20:22:16 +0530

    Create SecondMinimumNodeInABinaryTree.java

---
+++ SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {   
+       if(root == null) return -1;
+       if(root.left == null && root.right == null) return -1;
+    
+       int left = root.left.val;
+       int right = root.right.val;
+        
+       if(left == root.val) {
+           left = findSecondMinimumValue(root.left);
+       }
+       
+       if(right == root.val) {
+           right = findSecondMinimumValue(root.right);
+       }
+        
+       if(left != -1 && right != -1) {
+           return Math.min(left, right);
+       } else if(left != -1) {
+           return left;
+       } else {
+           return right;
+       }
+    }
+}
