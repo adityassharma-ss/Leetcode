3b12c704903d3cbbc4722e80cbc497fcf5eeba3f
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-11-04 23:29:13 +0530

    Create NumberOfIslands.java

---
+++ NumberOfIslands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(char[][] grid, int i, int j){
+        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j] != '1'){
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
