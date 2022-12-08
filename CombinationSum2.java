128467054e22c5f4ef487746d59237731c939d3b
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-12-08 18:43:08 +0530

    Create CombinationSum2.java

---
+++ CombinationSum2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) result.add(new ArrayList<>(tempList));
+        else {
+            for (int i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] == candidates[i-1]) continue;
+                tempList.add(candidates[i]);
+                backtrack(result, tempList, candidates, remain - candidates[i], i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
